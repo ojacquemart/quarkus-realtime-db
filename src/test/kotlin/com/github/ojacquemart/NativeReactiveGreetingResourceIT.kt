61809f985ec5ba52aa5e329eea5b827bf52347ba
Olivier Jacquemart <o.jacquemart@gmail.com>
2021-05-20 17:06:21 +0200

    chore: initial commit

---
+++ src/test/kotlin/com/github/ojacquemart/NativeReactiveGreetingResourceIT.kt
@@ -0,0 +1,6 @@
+package com.github.ojacquemart
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+class NativeReactiveGreetingResourceIT : ReactiveGreetingResourceTest()
---
+++ src/test/kotlin/com/github/ojacquemart/ReactiveGreetingResourceTest.kt
@@ -0,0 +1,20 @@
+package com.github.ojacquemart
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class ReactiveGreetingResourceTest {
+
+  @Test
+  fun testHelloEndpoint() {
+    given()
+      .`when`().get("/hello-resteasy-reactive")
+      .then()
+      .statusCode(200)
+      .body(`is`("Hello RESTEasy Reactive"))
+  }
+
+}
